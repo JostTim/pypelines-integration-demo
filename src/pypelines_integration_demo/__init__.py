c8f2fd0c9a194da2670326541946ff49a2099b32
JostTim <44769559+JostTim@users.noreply.github.com>
2024-11-08 08:16:33 +0100

    first push

--- src/pypelines_integration_demo/__init__.py
+++ src/pypelines_integration_demo/__init__.py
@@ -12,3 +12,14 @@ class TextPipe(BasePipe):
     class HashStep(BaseStep):
 
         step_name = "get_hashes"
+
+        def worker(self, session, *, extra=None):
+            print("hash")
+
+    class VirusCheckStep(BaseStep):
+
+        step_name = "get_virus_check"
+        requires = "process_text.get_hashes"
+
+        def worker(self, session, *, extra=None):
+            print("virus")
